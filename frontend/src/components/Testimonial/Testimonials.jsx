c9a60ab629941e4d5cafcaf31faeda261ccc986c
shivamgupta8418 <shivamguptab697@gmail.com>
2024-05-03 09:18:03 +0530

    changed feedbacks to english

--- frontend/src/components/Testimonial/Testimonials.jsx
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,104 +1,109 @@
-import React from 'react'
-import Slider from 'react-slick'
-import ava01 from '../../assets/images/ava-1.jpg'
-import ava02 from '../../assets/images/ava-2.jpg'
-import ava03 from '../../assets/images/ava-3.jpg'
+import React from "react";
+import Slider from "react-slick";
+import ava01 from "../../assets/images/ava-1.jpg";
+import ava02 from "../../assets/images/ava-2.jpg";
+import ava03 from "../../assets/images/ava-3.jpg";
 
 const Testimonials = () => {
-   const settings = {
-      dots:true,
-      infinite:true,
-      autoplay:true,
-      speed:1000,
-      swipeToSlide:true,
-      autoplaySpeed:2000,
-      slidesToShow:3,
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
 
-      responsive: [
-         {
-            breakpoint: 992,
-            settings: {
-               slidesToShow: 2,
-               slidesToScroll: 1,
-               infinite: true,
-               dots: true,
-            },
-            breakpoint: 576,
-            settings: {
-               slidesToShow: 1,
-               slidesToScroll: 1,
-               infinite: true,
-               dots: true,
-            },
-         }
-      ]
-   }
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
 
-   return <Slider {...settings}>
+  return (
+    <Slider {...settings}>
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+          I absolutely love this taxi booking app! It's incredibly user-friendly
+          and convenient, making it a breeze to book rides anytime, anywhere.
+          The interface is sleek and intuitive, allowing me to quickly input my
+          pick-up and drop-off locations, select my preferred vehicle type.
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava01} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>John Doe</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">John Doe</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+          Even schedule rides in advance with ease. What sets this app apart is
+          its reliability – I've never experienced any issues with late arrivals
+          or cancellations. The real-time tracking feature is fantastic,
+          providing peace of mind knowing exactly when my ride will arrive.
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava02} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>Lia Franklin</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">Lia Franklin</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+          Additionally, the driver ratings and reviews ensure that I always
+          receive top-notch service. The app also offers various payment
+          options, making transactions seamless and secure.
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava03} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>John Doe</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">John Doe</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+        Overall, I highly
+          recommend this taxi booking app to anyone looking for a stress-free
+          and efficient way to get around town. It's simply the best!I just love travelling with it.app also offers various payment
+          options, making transactions seamless and secure.
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava03} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>John Doe</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">John Doe</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
-   </Slider>
-}
+    </Slider>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
